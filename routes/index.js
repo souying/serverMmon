480fba92688345b6dcecd5d21eae05d1e3ab125f
MICE <100101065@qq.com>
2023-03-22 14:56:14 +0800

    数据同步优化

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+const NodeCache = require('node-cache');
+const cache = new NodeCache({ stdTTL: 60, checkperiod: 60 });
 // 获取配置
 const db = require('../common/data').serverList;
-let arrServer = []
+let serverData =  {};
 function createComprisonFunction(propertyName){
   return function(object1,object2){
       var value1 = object1[propertyName];
@@ -26,12 +28,12 @@ async function getServeOption(data) {
         headers: {
             "content-type": "application/json",
         },
-        // timeout:2500,
+        timeout:2600,
         body: JSON.stringify(data)
       }, function(error, response, body) {
         if (!error && response.statusCode == 200) {
             if(body.code==200){
-                arrServer.push({
+                serverData[data.url] = {
                   name:data.name,
                   location:data.location,
                   region:data.region,
@@ -39,29 +41,35 @@ async function getServeOption(data) {
                   id:data._id,
                   updata:data.updata,
                   data:body
-                })
+                }
+                
             }else{
-                arrServer.push({
+              if(!serverData[data.url]){
+                serverData[data.url] = {
                   name:data.name,
-                  location:data.location,
-                  region:data.region,
-                  getStatus:false,
-                  id:data._id,
-                  updata:data.updata,
-                  data:[]
-                })
+                    location:data.location,
+                    region:data.region,
+                    getStatus:false,
+                    id:data._id,
+                    updata:data.updata,
+                    data:[]
+                }
+              } 
             }
             
         }else{
-            arrServer.push({
+          if(!serverData[data.url]){
+            serverData[data.url] = {
               name:data.name,
-              location:data.location,
-              region:data.region,
-              getStatus:false,
-              id:data._id,
-              updata:data.updata,
-              data:[]
-            })
+                location:data.location,
+                region:data.region,
+                getStatus:false,
+                id:data._id,
+                updata:data.updata,
+                data:[]
+            }
+          }
+            
         }
         resolve()
         
@@ -87,20 +95,27 @@ if(timer!=undefined){
       
         Promise.all(promiseArr)
         .then(function () {
-            // console.log(arrServer);
+            for(let  i = 0; i < config.length; i++) {
+              arrServer.push(serverData[config[i].url])
+            }
             let data = arrServer.sort(createComprisonFunction("updata"));
             newData = JSON.stringify({code:200,data:data,updated:new Date().getTime()});
+            cache.set('data', newData);
         })
       } catch (error) {
         console.log(error)
       }
-  }, 2500)
+  }, 2800)
   
-var timer1 = undefined;
 
 router.post('/list', function(req, res, next) {
   try {
-    res.send(newData);
+    let cachedData = cache.get('data');
+    if (cachedData) {
+      res.send(cachedData);
+    }else{
+      res.send(newData);
+    }
   } catch (error) {
     console.log(error)
   }
